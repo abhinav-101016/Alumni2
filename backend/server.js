129b86a93a357344431ddab82b53c4ab553e0b9a
Abhinav-1016 <abhinav9129885700@gmail.com>
2026-03-04 15:21:52 +0530

    hhgh

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
 import profileRoutes from './routes/profile.js';
+import alumniRoutes from "./routes/alumni.js";
 
 dotenv.config();
 
